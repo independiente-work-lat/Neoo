d57c98cdfec225dd8fe417ec24a2c0d98a5017c9
Ihaveaproblem <136777376+independiente-work-lat@users.noreply.github.com>
2023-06-20 17:48:01 +0000

    Personalize

--- client/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-import abi from './Transactions.json'
-
-export const contractABI = abi.abi;
-export const contractAddress = '0x21545B9BdF31280a8F9793E9e71389333b493696'
--- client/src/utils/shortenAddress.js
+++
@@ -1,1 +0,0 @@
-export const shortenAddress = (address) => `${address.slice(0, 6)}...${address.slice(address.length - 6)}`;
